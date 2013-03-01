8046264492c0b60005c47ea5ae1db172a8eb66b5
Alexandru Mihai <the_snyper06@yahoo.com>
2013-03-02 00:49:03 +0200

    Stats design

--- statistics.php
+++ statistics.php
@@ -7,6 +7,10 @@
 	require_once '__TwigConfig.php';
 	require_once '__PermissionStudent.php';
 	
+	$questions = array(
+		0 => "Tipuri de investigatii: viziualizati o reprezentare grafica a impartirii pe categorii de investigatii a fiselor inscrise.",
+	);
+	
 	$template = $twig->loadTemplate('statistics.html');
-	echo $template->render(array('user' => $_SESSION['user']));
+	echo $template->render(array('user' => $_SESSION['user'], 'questions' => $questions));
 ?>
